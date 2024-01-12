ab980d2a3daf868830f2fd2c7b37c03a9fa689e6
Eslam Sife <eng.eslamsife@gmail.com>
2024-01-12 13:09:37 +0200

    init spring boot project for Emplouee Crud

---
+++ src/main/java/com/stc/viewemployee/crud/EmployeeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.stc.viewemployee.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+}
