b0cd7cdadf1d0e477a467082941408ba4d27eacd
EslamSife <eslamsife91@gmail.com>
2024-01-13 23:28:52 +0200

    add docker file and change the app name

--- src/main/java/com/stc/viewemployee/crud/EmployeePortalCrudApplication.java
+++ src/main/java/com/stc/viewemployee/crud/EmployeePortalCrudApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeCrudApplication {
+public class EmployeePortalCrudApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EmployeeCrudApplication.class, args);
+		SpringApplication.run(EmployeePortalCrudApplication.class, args);
 	}
 
 }
